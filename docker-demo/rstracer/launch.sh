ebe5ae7b573c50c45acde93c612ffc96e1da9358
VictorMeyer77 <=>
2024-11-30 20:08:21 +0100

    add demo docker compose

---
+++ docker-demo/rstracer/launch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# launch rstracer
+
+cd /rstracer || exit
+rstracer &
+
+# launch rstracer-dashboard
+
+cd /rstracer-dashboard || exit
+streamlit run rsdb.py --server.port="${STREAMLIT_PORT}"  &
+
+# request other containers randomly
+
+urls=("http://220.189.5.10:8501" "http://220.189.5.20:8502" "http://220.189.5.30:8503" "http://220.189.5.40:8504")
+while true; do
+  url_index=$((RANDOM % 4));
+  curl -s -m 1 "${urls[$url_index]}";
+  sleep 1;
+done &
+
+# launch copy tp azure
+
+cd /rstracer-azure || exit
+python3 -m rsaz &
+
+wait
