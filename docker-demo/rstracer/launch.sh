6fb8b1f73511d5bea715bf1121f7f929f29fbfde
VictorMeyer77 <=>
2024-12-01 18:04:38 +0100

    demo-docker: add new network

--- docker-demo/rstracer/launch.sh
+++ docker-demo/rstracer/launch.sh
@@ -12,9 +12,9 @@ streamlit run rsdb.py --server.port="${STREAMLIT_PORT}"  &
 
 # request other containers randomly
 
-urls=("http://220.189.5.10:8501" "http://220.189.5.20:8502" "http://220.189.5.30:8503" "http://220.189.5.40:8504")
+urls=("http://220.189.5.10:8501" "http://220.189.5.20:8502" "http://220.189.5.30:8503" "http://220.189.5.40:8504" "http://117.210.5.10:8505" "http://117.210.5.20:8506")
 while true; do
-  url_index=$((RANDOM % 4));
+  url_index=$((RANDOM % 6));
   curl -s -m 1 "${urls[$url_index]}";
   sleep 1;
 done &
